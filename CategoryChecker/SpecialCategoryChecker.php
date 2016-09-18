d3309e7639191262da51a8be54173c9ac62fafa6
Meneth <github@meneth.com>
2016-09-18 21:20:25 +0200

    Optimization
    Though still kinda slow on large wikis. Takes 4 or so second on one with
    1500 articles

--- CategoryChecker/SpecialCategoryChecker.php
+++ CategoryChecker/SpecialCategoryChecker.php
@@ -1,6 +1,10 @@
 <?php
 
 class SpecialCategoryChecker extends SpecialPage {
+	static $assessmentCategories = [];
+	static $versionCategories = [];
+	static $categorizationCategories = [];
+	
 	public function __construct() {
 		parent::__construct( 'CategoryChecker', 'autopatrol' );
 	}
@@ -14,65 +18,68 @@ class SpecialCategoryChecker extends SpecialPage {
 		$output = $this->getOutput();
 		$this->setHeaders();
 		
+		self::calcCategories();
+		
 		$dbr = wfGetDB( DB_SLAVE );
 		$res = $dbr->select(
 		"page",
-		["page_title", "page_id"],
+		"page_id",
 		[
 			"page_namespace" => "0",
 			"page_is_redirect" => "0"
 		]
 		);
 		
-		$output->addWikiText( "The following pages have no versioning category. Disambiguation, patch, wiki, and mod pages are excluded.\n" );
-		$out = "";
+		$versionOut = "The following pages have no versioning category. Disambiguation, patch, wiki, and mod pages are excluded.\n";
+		$assessmentOut = "\nThe following pages have no assessment.\n";
+		$categoryOut = "\nThe following pages are uncategorized (needing editing, verisoning, and hidden categories not considered).\n";
 		foreach( $res as $row ) {
 			$page = WikiPage::newFromID( $row->page_id );
 			
 			if ( self::showVersionPage( $page ) ) {
-				self::appendPage( $page, $out );
+				self::appendPage( $page, $versionOut );
 			}
-		}
-		$output->addWikiText( $out );
-		
-		$output->addWikiText( "\nThe following pages have no assessment.\n" );
-		$out = "";
-		foreach( $res as $row ) {
-			$page = WikiPage::newFromID( $row->page_id );
-			
 			if ( self::showAssessmentPage( $page ) ) {
-				self::appendPage( $page, $out );
+				self::appendPage( $page, $assessmentOut );
 			}
-		}
-		$output->addWikiText( $out );
-		
-		$output->addWikiText( "\nThe following pages are uncategorized (needing editing, verisoning, and hidden categories not considered).\n" );
-		$out = "";
-		foreach( $res as $row ) {
-			$page = WikiPage::newFromID( $row->page_id );
-			
 			if ( self::showCategoryPage( $page ) ) {
-				self::appendPage( $page, $out );
+				self::appendPage( $page, $categoryOut );
 			}
 		}
-		$output->addWikiText( $out );
+		$output->addWikiText( $versionOut );
+		$output->addWikiText( $assessmentOut );
+		$output->addWikiText( $categoryOut );
+	}
+	
+	static function calcCategories() {
+		self::calcSubCategories( "Articles by version", self::$versionCategories );
+		self::calcSubCategories( "Mods", self::$versionCategories );
+		self::$versionCategories[] = "Disambiguation";
+		self::$versionCategories[] = "Patches";
+		self::$versionCategories[] = "Wiki";
+		self::$versionCategories[] = "Mods";
+		
+		self::calcSubCategories( "Articles by quality", self::$assessmentCategories );
+		self::calcSubCategories( "Articles by version", self::$categorizationCategories );
+		self::calcSubCategories( "Need editing", self::$categorizationCategories );
+		self::calcSubCategories( "Hidden categories", self::$categorizationCategories );
+		self::calcSubCategories( "Status categories", self::$categorizationCategories );
+	}
+	
+	static function calcSubCategories( $category, array &$out ) {
+		$category = Category::newFromName( $category );
+		foreach( $category->getMembers() as $subCat ) {
+			if ( $subCat->getNamespace() == NS_CATEGORY ) {
+				$out[] = $subCat->getText();
+			}
+		}
 	}
 	
 	static function showVersionPage( WikiPage &$page ) {
 		$categories = $page->getCategories();
 		foreach( $categories as $category ) {
-			if ( $category->getText() == "Disambiguation"
-			|| $category->getText() == "Patches"
-			|| $category->getText() == "Wiki"
-			|| $category->getText() == "Mods" ) {
+			if ( in_array( $category->getText(), self::$versionCategories ) ) {
 				return false;
-			}
-			$categoryPage = WikiPage::factory( $category );
-			foreach( $categoryPage->getCategories() as $category2 ) {
-				if ( $category2->getText() == "Articles by version"
-				|| $category2->getText() == "Mods" ) {
-					return false;
-				}
 			}
 		}
 		return true;
@@ -85,11 +92,8 @@ class SpecialCategoryChecker extends SpecialPage {
 		}
 		$categories = $talkPage->getCategories();
 		foreach( $categories as $category ) {
-			$categoryPage = WikiPage::factory( $category );
-			foreach( $categoryPage->getCategories() as $category2 ) {
-				if ( $category2->getText() == "Articles by quality" ) {
-					return false;
-				}
+			if ( in_array( $category->getText(), self::$assessmentCategories ) ) {
+				return false;
 			}
 		}
 		return true;
@@ -98,16 +102,8 @@ class SpecialCategoryChecker extends SpecialPage {
 	static function showCategoryPage( WikiPage &$page ) {
 		$categories = $page->getCategories();
 		foreach( $categories as $category ) {
-			$categoryPage = WikiPage::factory( $category );
-			if ( !$categoryPage->getCategories()->valid() ) {
-				return false; // Assume uncategorized categories are valid
-			}
-			foreach( $categoryPage->getCategories() as $category2 ) {
-				if ( $category2->getText() != "Articles by version"
-				&& $category2->getText() != "Need editing"
-				&& $category2->getText() != "Hidden categories" ) {
-					return false;
-				}
+			if ( !in_array( $category->getText(), self::$categorizationCategories ) ) {
+				return false;
 			}
 		}
 		return true;
